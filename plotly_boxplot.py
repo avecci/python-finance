fd6fcf13b76552ac78fba27f9829f46a0313222c
Antti Väisänen <antti.vaisanen@norsunluutorni.fi>
2016-08-20 21:34:46 +0300

    Boxplot plotly-kirjastolla

---
+++ plotly_boxplot.py
@@ -0,0 +1,16 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import numpy as np
+
+y0 = np.random.randn(50)-1
+y1 = np.random.randn(50)+1
+
+trace0 = go.Box(
+    y=y0
+)
+trace1 = go.Box(
+    y=y1
+)
+data = [trace0, trace1]
+py.iplot(data)
