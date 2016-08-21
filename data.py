ff72e998db7dfc78a488ed4c386cd8ad050236a3
Antti Väisänen <antti.vaisanen@norsunluutorni.fi>
2016-08-21 19:02:00 +0300

    Kommentoitu ja poistettu turhat rivit

--- data.py
+++ data.py
@@ -1,11 +1,27 @@
+# Script to reshape data using pandas stack function.
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
+import xlrd
+import xlwt
+import matplotlib.pyplot as plt
 
-data = DataFrame(np.arange(6).reshape((2,3)), index=pd.Index(['Ohio', 'Colorado'], name='state'), columns=pd.Index(['one','two','three'],name='number'))
-
+# Test data stack using simple dataframe
+data 			= DataFrame(np.arange(6).reshape((2,3)), index=pd.Index(['Helsinki', 'Jyväskylä'], name='Kaupunki'), columns=pd.Index(['Yksi','Kaksi','Kolme'],name='Järjestys'))
 print(data);
 
-result = data.stack()
-
+result 			= data.stack()
 print(result);
+
+
+# Use data stack to reshape excel sheet data
+xls_file		= pd.ExcelFile('tyottomat.xls')
+table 			= xls_file.parse('Sheet1')
+print(table)
+
+pivotoitu 		= table.stack();
+exceliin 		= DataFrame(pivotoitu)
+print(pivotoitu)
+
+# Write output into a new Excel file:
+exceliin.to_excel('tyottomat2.xls')
