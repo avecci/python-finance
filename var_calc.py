5bcf943e7d0214f642f2c469a1b2f3848b35c8ad
Antti Väisänen <antti.vaisanen@norsunluutorni.fi>
2016-08-21 22:33:21 +0300

    Muotoiltu ja lisätty kommentteja koodiin

--- var_calc.py
+++ var_calc.py
@@ -13,10 +13,10 @@ from pygal.style import DefaultStyle
  
 from pygal import Config
 
-#Geometric Brownian motion
+##### Geometric Brownian motion
 S0 		= 100
 r 		= 0.05
-sigma 	= 0.25
+sigma	= 0.25
 T 		= 30 / 365
 I 		= 1000
 ST 		= S0* np.exp((r - 0.5 * sigma**2)*T + sigma * np.sqrt(T) * npr.standard_normal(I))
@@ -34,19 +34,20 @@ var = scs.scoreatpercentile(R_gbm, percs)
 print ("%16s  %16s" % ('Confidence level', 'Value-at-Risk'))
 print (33 * "-")
 
+# Print VaR values and probabilities
 for pair in zip (percs, var):
 	print ("%16.2f %16.3f" % (100- pair[0], -pair[1]))
 
 
-# Jump diffusion
+##### Jump diffusion
 M 		= 50
-lamb 	= 0.75
+lamb	= 0.75
 mu 		= -0.6
-delta 	= 0.25
+delta	= 0.25
 dt 		= 30/365/M
 rj 		= lamb * (np.exp(mu + 0.5*delta**2)-1)
 S 		= np.zeros((M + 1, I))
-S[0] 	= S0
+S[0]	= S0
 
 sn1 	= npr.standard_normal((M + 1, I))
 sn2 	= npr.standard_normal((M + 1, I))
@@ -67,6 +68,8 @@ plt.close()
 # Plotting JD and GBM in same figure
 percs 	= [0.01, 0.1, 1.0, 2.5, 5.0, 10.0]
 var 	= scs.scoreatpercentile(R_jd, percs)
+
+# Print VaR values and probabilities
 print ("%16s %16s" % ('Confidence level', 'Value-at-Risk'))
 print (33 * "-")
 
@@ -89,11 +92,11 @@ jd_var 	= scs.scoreatpercentile(R_jd, percs)
 #plt.savefig('var_total.png')
 
 
-line_chart = pygal.Line(human_readable=True, style=DefaultStyle)
-line_chart.title = 'Value-At-Risk for geometric Brownian motion and jump diffusion'
-line_chart.x_title = "100 - confidence level [%]"
-line_chart.y_title = "Value-A-Risk"
-line_chart.x_labels = percs
+line_chart			= pygal.Line(human_readable=True, style=DefaultStyle)
+line_chart.y_title	= 'Value-At-Risk for geometric Brownian motion and jump diffusion'
+line_chart.x_title	= "100 - confidence level [%]"
+line_chart.y_title	= "Value-A-Risk"
+line_chart.x_labels	= percs
 line_chart.add('Geometric Brownian Motion', gbm_var)
 line_chart.add('Jump Diffusion',  jd_var)
 line_chart.show_legend=True
