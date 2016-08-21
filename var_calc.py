7886f49ef67197dc32c11be859bc398d2224912b
Antti Väisänen <antti.vaisanen@norsunluutorni.fi>
2016-08-21 21:57:41 +0300

    Lisätty VaR-results ja var_calc

--- var_calc.py
+++ var_calc.py
@@ -1,5 +1,5 @@
-#Value-at-Risk simulation using geometric Brownian movement and jump diffusion
-# Revised from Python for Finance by Yves Hilpisch (O'Reilly). Copyright 2015 Yves Hilpisch, 978-1-491-94528-5.
+#Value-at-Risk simulation using geometric Brownian motion and jump diffusion
+# Adapted from Python for Finance by Yves Hilpisch (O'Reilly). Copyright 2015 Yves Hilpisch, 978-1-491-94528-5.
 
 import numpy as np
 import numpy.random as npr
@@ -8,7 +8,12 @@ import seaborn as sns
 import scipy.stats as scs
 from scipy.stats import norm
 
-#Geometric Brownian movement
+import pygal
+from pygal.style import DefaultStyle
+ 
+from pygal import Config
+
+#Geometric Brownian motion
 S0 		= 100
 r 		= 0.05
 sigma 	= 0.25
@@ -17,16 +22,9 @@ I 		= 1000
 ST 		= S0* np.exp((r - 0.5 * sigma**2)*T + sigma * np.sqrt(T) * npr.standard_normal(I))
 R_gbm 	= np.sort(ST-S0)
 
-# Plot GBM
-#plt.hist(R_gbm, bins=50)
-#plt.xlabel('absolute return')
-#plt.ylabel('frequency')
-#plt.grid(True) 
-#plt.savefig('var1.png');
-#plt.close()
 
 # Plot distribution plot for GBM:
-hist_seaborn = sns.distplot(R_gbm, bins=50, fit=norm, color="Blue")
+hist_seaborn = sns.distplot(R_gbm, bins=50, fit=norm, color="Red")
 hist_seaborn.set(ylabel="Density", xlabel="Absolute Return")
 hist_seaborn.figure.savefig('var1.png')
 plt.close()
@@ -61,7 +59,7 @@ for t in range(1, M + 1, 1):
 R_jd = np.sort(S[-1] - S0)
 
 # Plot distribution plot JD:
-hist_seaborn = sns.distplot(R_jd, bins=50, fit=norm, color="green")
+hist_seaborn = sns.distplot(R_jd, bins=50, fit=norm, color="Blue")
 hist_seaborn.set(ylabel="Density", xlabel="Absolute Return")
 hist_seaborn.figure.savefig('var2.png')
 plt.close()
@@ -80,13 +78,23 @@ gbm_var = scs.scoreatpercentile(R_gbm, percs)
 jd_var 	= scs.scoreatpercentile(R_jd, percs)
 
 
-# Plotataan GBM ja JD samaan kuvaajaan
-plt.plot(percs, gbm_var, 'b', lw=1.5, label='Geometric Brownian Movement', color="Blue")
-plt.plot(percs, jd_var, 'r', lw=1.5, label='Jump Diffusion', color="Green")
-plt.legend(loc=4)
-plt.xlabel('100 - confidence level [%]')
-plt.ylabel('Value-At-Risk')
-plt.grid(True)
-plt.ylim(ymax=0.0)
-plt.savefig('var_total.png')
+# Plot GBM and JD values in same figure
+#plt.plot(percs, gbm_var, 'b', lw=1.5, label='Geometric Brownian Motion', color="Red")
+#plt.plot(percs, jd_var, 'r', lw=1.5, label='Jump Diffusion', color="Blue")
+#plt.legend(loc=4)
+#plt.xlabel('100 - confidence level [%]')
+#plt.ylabel('Value-At-Risk')
+#plt.grid(True)
+#plt.ylim(ymax=0.0)
+#plt.savefig('var_total.png')
 
+
+line_chart = pygal.Line(human_readable=True, style=DefaultStyle)
+line_chart.title = 'Value-At-Risk for geometric Brownian motion and jump diffusion'
+line_chart.x_title = "100 - confidence level [%]"
+line_chart.y_title = "Value-A-Risk"
+line_chart.x_labels = percs
+line_chart.add('Geometric Brownian Motion', gbm_var)
+line_chart.add('Jump Diffusion',  jd_var)
+line_chart.show_legend=True
+line_chart.render_to_file('var_total.svg')
